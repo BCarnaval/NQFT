b17d19a86d99c2c22bc12d972cf68912b980ba67
BCarnaval <antoinedelagrave@hotmail.com>
2022-09-23 15:35:40 -0400

    Test ignore

--- tests/test_qft.py
+++ tests/test_qft.py
@@ -1,12 +1,12 @@
 from qft import __version__
-from qft.hamiltonian import funcs
+from qft.hamiltonian._funcs import Network
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_hamiltonian():
     sites = 4
-    network = funcs.Network(sites_nb=sites)
+    network = Network(sites_nb=sites)
     H = network.get_hamiltonian(model="Hubbard", U=1, t=1)
 
     assert H.shape == (4**sites, 4**sites)
