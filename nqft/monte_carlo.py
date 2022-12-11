dcc517ebb4c8c19db0d0a14a0f2337fe8125b436
BCarnaval <antoinedelagrave@hotmail.com>
2022-12-11 17:27:15 -0500

    Monte-Carlo hamiltonian fixed

--- nqft/monte_carlo.py
+++ nqft/monte_carlo.py
@@ -1,3 +1,6 @@
+"""Docs
+"""
+
 import numpy as np
 from scipy.signal import convolve2d
 
@@ -7,8 +10,9 @@ def build_h(shape: tuple[int], mu: float, hops: np.array) -> np.ndarray:
     """
     t, tp, tpp = hops
     sites = shape[0] * shape[1]
-    h = np.ones(shape=(sites, sites))
-    t_ij = np.array([
+    cluster = np.zeros(shape=shape)
+    h = np.zeros(shape=(sites, sites))
+    t_ij_kernel = np.array([
         [0, 0, tpp, 0, 0],
         [0, tp, t, tp, 0],
         [tpp, t, mu, t, tpp],
@@ -16,20 +20,16 @@ def build_h(shape: tuple[int], mu: float, hops: np.array) -> np.ndarray:
         [0, 0, tpp, 0, 0]
     ])
 
-    h = convolve2d(h, t_ij, mode='same', boundary='wrap')
-    # for coord_1, i in np.ndenumerate(cluster):
-    #     x1, y1 = coord_1
-    #     for coord_2, j in np.ndenumerate(cluster):
-    #         x2, y2 = coord_2
-    #         dist = ((x1 - x2)**2 + (y1 - y2)**2)**(1/2)
-    #         if dist > 2.0:
-    #             h[i, j] = 0.0
-    #         elif dist == 2.0:
-    #             h[i, j] = hops[2]
-    #         elif dist == (2.0)**(1/2):
-    #             h[i, j] = hops[1]
-    #         elif dist == 1.0:
-    #             h[i, j] = hops[0]
+    column = 0
+    for coords in np.ndindex(shape):
+        i, j = coords
+        cluster[i, j] = 1.0
+
+        h[:, column] = convolve2d(
+            cluster, t_ij_kernel, mode='same', boundary='wrap').ravel()
+
+        cluster[i, j] = 0.0
+        column += 1
 
     return h
 
